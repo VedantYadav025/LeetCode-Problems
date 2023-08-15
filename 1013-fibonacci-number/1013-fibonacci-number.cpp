3379253a55fa9a23543eb7b7e0b967454a1d2e6b
VedantYadav025 <136351429+VedantYadav025@users.noreply.github.com>
2023-08-15 13:51:26 +0530

    Time: 14 ms (36.14%), Space: 6 MB (24.4%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,21 +1,8 @@
 class Solution {
 public:
     int fib(int n) {
-        int dp[n + 1];
-        for (int i = 0; i < n + 1; i++){
-            dp[i] = -1;
-        }
-        return helper(n, dp);
-    }
-private:
-    int helper(int n, int* dp){
-        if (n == 0 || n == 1){
+        if (n == 0 || n == 1)
             return n;
-        }
-        if (dp[n] != -1){
-            return dp[n];
-        }
-        dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
-        return dp[n];
+        return fib(n - 1) + fib(n - 2);
     }
 };
